5125c3ddb08aa74bcf9c6220b94cbaebaaa837a9
Ed Kademan <ed@kademan.org>
2017-11-14 15:49:11 -0500

    load jquery

--- html/foobar.js
+++ html/foobar.js
@@ -1,1 +1,8 @@
+function load_jquery() {
+  var j  = document.createElement("script");
+  j.type = "text/javascript";
+  j.src  = "jquery.js";}
+
 function foo() {return "foo for you";}
+
+load_jquery()
