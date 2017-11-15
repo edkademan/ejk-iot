d81dba04b4a2b704ffa19f021824bd7da5af2bfa
Ed Kademan <ed@kademan.org>
2017-11-15 09:14:57 -0500

    more load jquery

--- html/foobar.js
+++ html/foobar.js
@@ -1,8 +1,19 @@
+var _jquery_available = false;
+
 function load_jquery() {
-  var j  = document.createElement("script");
-  j.type = "text/javascript";
-  j.src  = "jquery.js";}
+  var cb = function() {
+    alert("jquery is loaded");
+    _jquery_available = true;}
+  var j    = document.createElement("script");
+  j.type   = "text/javascript";
+  j.src    = "jquery.js";
+  j.onload = cb;
+  document.getElementsByTagName("head")[0].appendChild(j);}
 
-function foo() {return "foo for you";}
+function foo() {
+  if(_jquery_available)
+    alert("jquery is available");
+  else
+    alert("jquery is not available");}
 
-load_jquery()
+load_jquery();
