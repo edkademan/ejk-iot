a811e2ecad2a776eb153458e43f39897e461e3fd
Ed Kademan <ed@kademan.org>
2017-11-15 09:49:19 -0500

    simple jquery load

--- html/foobar.js
+++ html/foobar.js
@@ -1,19 +1,9 @@
-var _jquery_available = false;
-
-function load_jquery() {
-  var cb = function() {
-    alert("jquery is loaded");
-    _jquery_available = true;}
-  var j    = document.createElement("script");
-  j.type   = "text/javascript";
-  j.src    = "jquery.js";
-  j.onload = cb;
-  document.getElementsByTagName("head")[0].appendChild(j);}
-
-function foo() {
-  if(_jquery_available)
-    alert("jquery is available");
-  else
-    alert("jquery is not available");}
-
-load_jquery();
+function Foobar() {
+  Foobar.prototype.load_jquery = function() {
+    var j    = document.createElement("script");
+    j.type   = "text/javascript";
+    j.src    = "jquery.js";
+    document.getElementsByTagName("head")[0].appendChild(j);
+    return j;}
+  Foobar.prototype.run_on_ready = function(k) {
+    this.load_jquery().onload = k;}}
